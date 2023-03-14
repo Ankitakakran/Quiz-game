8c140695274b0f6a2ed0a5b40b31626cd8f358d1
Ankitakakran <127766135+Ankitakakran@users.noreply.github.com>
2023-03-14 23:18:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+score = 0
+
+answer = input("What does CPU stands for? ")
+if answer == "Central processing unit":
+    print("Correct! ")
+    score += 1
+else:
+    print("Incorrect! ")
+
+answer = input("What does GPU stands for? ")
+if answer == "Graphics processing unit":
+    print("Correct! ")
+    score += 1
+else:
+    print("Incorrect! ")
+
+answer = input("What does RAM stands for? ")
+if answer == "Random access memory":
+    print("Correct! ")
+    score += 1
+else:
+    print("Incorrect! ")
+
+answer = input("What does PSU stands for? ")
+if answer == "Power supply":
+    print("Correct! ")
+    score += 1
+else:
+    print("Incorrect! ")
+
+print("You got " + str(score) + " questions correct! ")
